3214429989170d53c8a97c052e175f24d72fd971
Percival Phakathi <percivalphakathi@Percivals-MacBook-Pro.local>
2024-05-25 18:05:46 +0200

    huber regressor

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def update_plot(X_outliers, y_outliers):
     )
 
 # Streamlit interface
-st.markdown("<h1 style='text-align: center;'>Comparison of OLS and Huber Regression with Outliers</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>Comparison of Linear and Huber Regression with Outliers</h1>", unsafe_allow_html=True)
 st.markdown("<p style='text-align: center;'>Welcome to the regression comparison dashboard!</p>", unsafe_allow_html=True)
 
 X_outliers = st.text_input("Enter X outliers (comma-separated):", "")
